fafc66249fbbbe5f08286381762517a5c48aa417
edpomacedo <edpomacedo@gmail.com>
2023-12-17 20:44:43 -0300

    readme

--- feeds.py
+++ feeds.py
@@ -1,6 +1,6 @@
 # feeds.py
 
 feeds = [
-    {"name": "STJ - Notícias", "url": "https://res.stj.jus.br/hrestp-c-portalp/RSS.xml"},
+    {"name": "STJ - Notícias", "url": "https://res.stj.jus.br/hrestp-c-portalp/RSS.xml"}
     # Adicione mais feeds conforme necessário
 ]
